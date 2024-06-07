eabb7b29a57153271e1aeb88414dfb57f6444795
GD757 <garydunnington@gmail.com>
2024-06-07 18:01:12 -0400

    Implement Internet Search

---
+++ internet_search.py
@@ -0,0 +1,15 @@
+import webbrowser
+
+url = 'https://www.memedroid.com/'
+
+# MacOS
+chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+
+# Windows
+#webbrowser.get("C:\Program Files\Google\Chrome\Application\chrome.exe %s").open("http://google.com")
+
+
+# Linux
+# chrome_path = '/usr/bin/google-chrome %s'
+
+
